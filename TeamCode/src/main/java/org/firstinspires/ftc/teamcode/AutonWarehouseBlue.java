e92769e6a7358246fdfeff9d26fc6d366b53df4a
robotabc773 <sebastianxwu@gmail.com>
2022-02-19 17:54:22 -0500

    Update autons

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonWarehouseBlue.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Warehouse - Blue")
+public class AutonWarehouseBlue extends BaseAuton
+{
+    @Override
+    public void runOpMode()
+    {
+        super.runOpMode();
+
+        waitForStart();
+
+        ElapsedTime runtime = new ElapsedTime();
+
+        encoderDrive(0.2, 22, 22, 7);
+        encoderDrive(0.2 , 11, -11, 5);
+        runtime.reset();
+        while(opModeIsActive() && runtime.seconds() < 3)
+        {
+            double power = 0.4;
+            robot.leftFront.setPower(power);
+            robot.leftBack.setPower(power);
+            robot.rightFront.setPower(power);
+            robot.rightBack.setPower(power);
+        }
+        robot.leftFront.setPower(0);
+        robot.leftBack.setPower(0);
+        robot.rightFront.setPower(0);
+        robot.rightBack.setPower(0);
+    }
+}
